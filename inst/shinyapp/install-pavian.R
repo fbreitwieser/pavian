b34c2d0790ff5bdd5a2f4601d6c679e3e20b3517
Florian Breitwieser <florian.bw@gmail.com>
2016-11-18 11:09:31 -0500

    Update install-pavian.R

--- inst/shinyapp/install-pavian.R
+++ inst/shinyapp/install-pavian.R
@@ -1,18 +1,26 @@
 
 options(warn=2)
 
-install.packages(c("devtools", "htmlwidgets", "magrittr", "Rcpp",))
+if (!require(Rsamtools)) {
+  message("Installing Rsamtools from Bioconductor")
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("Rsamtools")
+}
+
+all_packages <- c("devtools", "htmlwidgets", "magrittr", "Rcpp","shiny",
+                  "shinydashboard","shinyjs","shinyBS","sparkline","DT",
+                  "rhandsontable","sankeyD3","scatterD3","ggplot2",
+                  "htmltools","plyr","dplyr","yaml")
+
+packages_to_install <- all_packages[!all_packages %in% rownames(installed.packages())]
+
+message("Installing ",length(all_packages), " required packages")
+install.packages(packages_to_install)
+
+message("Installing sankeyD3 from https://github.com/fbreitwieser/sankeyD3")
 devtools::install_github("sankeyD3")
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("Rsamtools")
-
-install.packages(c("shiny","shinydashboard","shinyjs","shinyBS",
-                   "sparkline","DT","rhandsontable",
-                   "sankeyD3","scatterD3",
-                   "ggplot2","htmltools",
-                   "plyr","dplyr","yaml"))
-
+message("Installing pavian from https://github.com/fbreitwieser/pavian")
 devtools::install_github("fbreitwieser/pavian")
 
 message("You successfully installed pavian v",packageVersion("pavian"),", congratulations !\n")
